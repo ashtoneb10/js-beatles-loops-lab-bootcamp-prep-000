a6c63fe7ca88540328b543e395356cc01392bf94
Ashton Blair <ashtoneb10@aol.com>
2018-01-28 01:53:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function iLoveTheBeatles(number){
   do {
     array.push("I love the Beatles!");
     number = number -1;
-  } while (number <15);
+  } while (number <15 && number > 0);
   return array;
 }
